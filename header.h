b250fb6de0c3896cc3abe7f9cb3639e803f12c59
ahyen <ahyen@naver.com>
2017-12-05 15:09:29 +0900

    Create header.h

---
+++ header.h
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void stringcpy(char *str, char *a, int n); // 문자열 str을 a에 복사하는 함수
+char *triv(char* str, int d, int n);
+char *juggling(char *arr,int N, int d);
+char *block_swap(char *str, int n, int d);
+char *reversal(char *str, int n, int d);
