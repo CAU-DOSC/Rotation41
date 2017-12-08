de35f03f0b2a186061585a6db671f7fac1ca9e60
seongyeon38 <seongyeon38@naver.com>
2017-12-08 23:10:03 +0900

    Update triv.c

--- triv.c
+++ triv.c
@@ -2,21 +2,18 @@
 
 char *triv(char* str, int d, int n)
 {
-	char save_str = { 0 };
 	int cnt = 0;
 	if (d < 0)
 	{
 		d = 0 - d;
 		for (int cnt = 0; cnt < d; cnt++)
 		{
-			save_str = str[0];
+			str[n - 1] = str[0];
 
 			for (int j = 0; j < n-1; j++)
 			{
 				str[j] = str[j + 1];
 			}
-
-			str[n - 1] = save_str;
 		}
 	}
 
@@ -24,14 +21,13 @@ char *triv(char* str, int d, int n)
 	{
 		for (int cnt = 0; cnt < d; cnt++)
 		{
-			save_str = str[n - 1];
+			str[0] = str[n - 1];
 
 			for (int j = n - 1; j >0; j--)
 			{
 				str[j] = str[j - 1];
 			}
-
-			str[0] = save_str;
+		
 		}
 	}
 	return str;
